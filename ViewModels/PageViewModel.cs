894fcbb8986231e370178ffffd9b84d4a9baf58c
Alen767 <masis.sis.04@bk.ru>
2023-10-27 15:10:13 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/PageViewModel.cs
@@ -0,0 +1,101 @@
+﻿using BusinessLogicLayer.Services;
+using Chess.Views;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Chess.ViewModels
+{
+    public class PageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private MainPage mainPage;
+        private GamePage gamePage;
+        private Page currentPage;
+
+        public Page CurrentPage
+        {
+            get => currentPage;
+            set
+            {
+                currentPage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand StartGame
+        {
+            get => new RelayCommand(() => {
+                CurrentPage = gamePage;
+                gamePage.GameTime = mainPage.GameTime;
+                ChessGame.StartGame();
+            });
+        }
+
+        public ICommand ContinueGame
+        {
+            get => new RelayCommand(() =>
+            {
+                CurrentPage = gamePage;
+                (mainPage.continueButton.Parent as Border).Visibility = Visibility.Hidden;
+                ChessGame.ContinueGame();
+            });
+        }
+
+        public ICommand OpenRules
+        {
+            get => new RelayCommand(() => {
+            });
+        }
+
+        public ICommand GoBack
+        {
+            get => new RelayCommand(() => {
+                CurrentPage = mainPage;
+                if (ChessGame.IsSavedGame)
+                {
+                    (mainPage.continueButton.Parent as Border).Visibility = Visibility.Visible;
+                }
+                else (mainPage.continueButton.Parent as Border).Visibility = Visibility.Hidden;
+            });
+        }
+
+        public PageViewModel()
+        {
+            mainPage = new MainPage();
+            gamePage = new GamePage();
+
+            mainPage.playButton.Command = StartGame;
+            mainPage.continueButton.Command = ContinueGame;
+
+            CurrentPage = mainPage;
+        }
+
+        public void UpdateLocalization(string local)
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(local);
+            Properties.Settings.Default.Culture = local;
+            Properties.Settings.Default.Save();
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string property = null)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using BusinessLogicLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chess.Views
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public int GameTime { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeCulture();
+            InitializeGameTime();
+            InitializeContinueButtonVisibility();
+        }
+
+        private void InitializeCulture()
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(Properties.Settings.Default.Culture);
+        }
+
+        private void InitializeGameTime()
+        {
+            GameTime = Properties.Settings.Default.Time;
+            timeTextBlock.Text = $"{GameTime}";
+        }
+
+        private void InitializeContinueButtonVisibility()
+        {
+            if (ChessGame.IsSavedGame)
+            {
+                (continueButton.Parent as Border).Visibility = Visibility.Visible;
+            }
+        }
+
+        private void upNum_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            GameTime = Math.Min(GameTime + 5, 30);
+            ShowTimeAndSave();
+        }
+
+        private void downNum_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            GameTime = Math.Max(GameTime - 5, 5);
+            ShowTimeAndSave();
+        }
+
+        private void ShowTimeAndSave()
+        {
+            timeTextBlock.Text = $"{GameTime}";
+            Properties.Settings.Default.Time = (byte)GameTime;
+            Properties.Settings.Default.Save();
+        }
+        // Создание экземпляра Image из файла изображения
+
+        // Присвоение изображения в качестве фонового изображения элементу управления
+
+    }
+}
